6288e80faf23d89ded2dd3923388858be168a9d8
Artem <af.fozie@gmail.com>
2020-12-21 17:49:13 +0300

    complete chart

--- index.js
+++ index.js
@@ -1,25 +1,105 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const avatarBtn = document.querySelector('.user__avatar-btn')
-  const avatar = document.querySelector('.user__avatar')
-  const resBtn = document.querySelector('.results-item')
+const ctx = document.getElementById('main-chart').getContext('2d')
 
-  avatarBtn.addEventListener('change', e => {
-    console.log(e.target.files[0])
-    // new Cropper(avatar, {
-    //   aspectRatio: 1 / 1,
-    //   crop(event) {
-    //     console.log(event.detail.x)
-    //     console.log(event.detail.y)
-    //     console.log(event.detail.width)
-    //     console.log(event.detail.height)
-    //     console.log(event.detail.rotate)
-    //     console.log(event.detail.scaleX)
-    //     console.log(event.detail.scaleY)
-    //   },
-    // })
-  })
+function getRandonArr(lenght) {
+  const randomArr = [0]
+  for (let i = 0; i < lenght; i++) {
+    let randomNum = Math.floor(Math.random() * 10) + 1 + ''
+    randomArr.push(+randomNum)
+  }
+  return randomArr
+}
 
-  resBtn.addEventListener('click', () => {
-    console.log(avatarBtn.files[0])
-  })
+const chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+  // The data for our dataset
+  data: {
+    datasets: [
+      {
+        label: 'Участвует в мероприятиях',
+        fill: false,
+        borderWidth: '2',
+        borderColor: '#ED5338',
+        backgroundColor: '#fff',
+        pointRadius: 0,
+        data: getRandonArr(3),
+      },
+      {
+        label: 'Участвует в проектах',
+        fill: false,
+        borderWidth: '2',
+        borderColor: '#2EB349',
+        backgroundColor: '#fff',
+        pointRadius: 0,
+        data: getRandonArr(3),
+      },
+      {
+        label: 'Инвестирует в проектах',
+        fill: false,
+        borderWidth: '2',
+        borderColor: '#2A99FF',
+        backgroundColor: '#fff',
+        pointRadius: 0,
+        data: getRandonArr(3),
+      },
+    ],
+    labels: ['', '', '', ''],
+  },
+
+  // Configuration options go here
+  options: {
+    responsive: true,
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          display: false,
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+    },
+    legend: {
+      display: true,
+      position: 'bottom',
+      align: 'start',
+      fullWidth: true,
+      labels: {
+        fontFamily: '"Proxima Nova", sans-serif',
+        fontSize: 14,
+        boxWidth: 4,
+        fontColor: '#111B42',
+        usePointStyle: true,
+      },
+    },
+  },
 })
+
+// const avatarBtn = document.querySelector('.user__avatar-btn')
+// const avatar = document.querySelector('.user__avatar')
+// const resBtn = document.querySelector('.results-item')
+// avatarBtn.addEventListener('change', e => {
+//   console.log(e.target.files[0])
+//   new Cropper(avatar, {
+//     aspectRatio: 1 / 1,
+//     // crop(event) {
+//     //   console.log(event.detail.x)
+//     //   console.log(event.detail.y)
+//     //   console.log(event.detail.width)
+//     //   console.log(event.detail.height)
+//     //   console.log(event.detail.rotate)
+//     //   console.log(event.detail.scaleX)
+//     //   console.log(event.detail.scaleY)
+//     // },
+//   })
+// })
+// resBtn.addEventListener('click', () => {
+//   console.log(avatarBtn.files[0])
+// })
